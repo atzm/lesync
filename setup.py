e5ae4aefb386ef650d496781f99152821a867577
atzm <atzm@atzm.org>
2017-11-18 21:54:12 +0900

    setup: install lehash as executable

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ setup(
     author='Atzm WATANABE',
     author_email='atzm@atzm.org',
     license='BSD-2',
-    entry_points={'console_scripts': ['lesync = lesync:main']},
+    entry_points={'console_scripts': [
+        'lesync = lesync:main',
+        'lehash = lehash:main',
+    ]},
     py_modules=['lesync', 'lehash'],
     install_requires=[],
     classifiers=[
